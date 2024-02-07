a4d7856ca8a87b6dfeb32914b2638b881347fc2b
lai2-24 <lai2-24@itismeucci.com>
2024-02-07 13:48:21 +0100

    init

--- index.php
+++ index.php
@@ -4,12 +4,31 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+require_once("Classe.php");
+
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Ciao sei nell'index");
     return $response;
+});
+$app->get('/alunni', function (Request $request, Response $response, $args) {
+    $vcia = new Classe("5CIA");
+    $allAlunni = $vcia->getAllAlunni();
+    $response->getBody()->write($allAlunni);
+    return $response;
+});
+$app->get('/alunni/{name}', function (Request $request, Response $response, $args) {
+    $vcia = new Classe("5CIA");
+    $alunno = $vcia->getAlunno($args["name"]);
+    if(is_null( $alunno )) {
+        $response->getBody()->write("ciao");
+    } else {
+        $response->getBody()->write($alunno->allInfo());
+    }
+    return $response;
+    
 });
 
 $app->run();
